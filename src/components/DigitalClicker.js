e2e899d7f02900465f91018f999a22933cda0049
Artur <info@aigroupusa.com>
2022-02-17 00:05:44 -0500

    commit tree 3e32d7141b0051ffc58e2abea58c11d1468dc356

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,34 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component {
+
+    constructor() {
+        super();
+    
+        // Initial state is defined
+        this.state = {
+            timesClicked: 0,
+        };
+      }
+
+    handleClick = () => {
+        // debugger;
+        this.setState(previousState => {
+          return {
+            timesClicked: previousState.timesClicked + 1
+          }
+        })
+      }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.timesClicked}</h1>
+                <button onClick={this.handleClick} >{this.state.timesClicked}</button>
+            </div>
+        );
+    }
+}
+
+export default DigitalClicker;
